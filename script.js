e7f559f156d7bb96c9558eab25c737ae41f6b88d
Irina Shaporova <ira.shaporova@mail.ru>
2023-08-18 12:35:49 +0300

    fix: delete

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ document.querySelector('.burger-menu').addEventListener('click' , function() {
     this.classList.toggle('active');
     document.querySelector('nav').classList.toggle('open');
     });
-    console.log(ghbdn)
+  
